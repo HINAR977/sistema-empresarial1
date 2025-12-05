1b4259be139060d8adb9559886f62c1683b6ef8a
HINAR977 <frreezmb@gmail.com>
2025-12-05 11:48:15 -0500

    Actualización de archivos modificados

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 function login() {
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
-
     let destino = "";
 
     if (user === "bodegaUser" && pass === "bodega123") destino = "bodega.html";
@@ -9,10 +8,8 @@ function login() {
     if (user === "contaUser" && pass === "conta123") destino = "contabilidad.html";
     if (user === "ventasUser" && pass === "ventas123") destino = "ventas.html";
 
-    if (destino === "") {
-        alert("Usuario o contraseña incorrectos");
-        return;
-    }
+    if (!destino) { alert("Usuario o contraseña incorrectos"); return; }
 
+    localStorage.setItem('usuarioActual', user); // <- IMPORTANTE
     window.location.href = destino;
 }
